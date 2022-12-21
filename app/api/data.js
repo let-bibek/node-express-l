2fa4fe9036a697504b1cdb8f82500592a2d415db
let_bibek <kumarbibek094@gmail.com>
2022-12-21 18:49:05 +0545

    simple api call

--- app/api/data.js
+++ app/api/data.js
@@ -1,2 +1,8 @@
 const users={name:["bibek shrestha",'riya shrestha','yoddha','lukas moddo'],age:[21,19,30,17]}
-module.exports=users;
+const workers = [
+  { id: 1, username: "rm15", email: "demo@demo.com" },
+  { id: 2, username: "rox85", email: "rox@demo.com" },
+  { id: 3, username: "brick7", email: "brick@demo.com" },
+  { id: 4, username: "sort2007", email: "sort@demo.com" },
+];
+module.exports = workers;
--- app/apicall.js
+++ app/apicall.js
@@ -1,13 +1,9 @@
 const http=require("http");
+const data=require("./api/data");
 const { stringify } = require("querystring");
 http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"application/json"})
     res.write(
-      JSON.stringify({
-        name: "bibek shrestha",
-        age: 21,
-        email: "bibek@softdrax.com",
-      })
-    );
+      JSON.stringify(data));
     res.end();
 }).listen(8200);
