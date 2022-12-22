60209633c375ba5b39c1c62fe36dea9da0100fb5
let_bibek <kumarbibek094@gmail.com>
2022-12-22 20:28:11 +0545

    create file in certain directory

--- app/showFileList.js
+++ app/showFileList.js
@@ -1,7 +1,12 @@
-const {read,write}=require('fs');
+const fs = require("fs");
 const path=require('path');
 const dir=path.join(__dirname,'files');
 for(i=0;i<5;i++){
-    write.writeFileSync(dir + `/file_${i}.txt`, "Fox is clever");
+    fs.writeFileSync(dir + `/file_${i}.txt`, "Fox is clever");
 }
 
+fs.readdir(dir, (err, file) => {
+ file.forEach((f)=>{
+    console.log(`file name is : ${f}`);
+ })
+});
