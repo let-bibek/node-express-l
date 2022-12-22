5df3d69d67e90d57f91af9a9ad4865ea9d6f4968
let_bibek <kumarbibek094@gmail.com>
2022-12-22 21:14:55 +0545

    read file

--- app/crudOpWithFileSys.js
+++ app/crudOpWithFileSys.js
@@ -1,5 +1,11 @@
 const fs=require('fs');
 const path=require('path');
 const dirPath=path.join(__dirname,'crud');
+// app/crud/demo.txt
 const filePath=`${dirPath}/demo.txt`;
-fs.writeFileSync(filePath,'This is xerox copy.')
+// write
+fs.writeFileSync(filePath, "This is xerox copy.");
+// read
+fs.readFile(filePath, 'utf8', (err, f) => {
+  console.log(f);
+});
