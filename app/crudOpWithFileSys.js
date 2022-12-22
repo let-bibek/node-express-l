d694fc2f1c6823c7e020df1af24159efe5a211c3
let_bibek <kumarbibek094@gmail.com>
2022-12-22 22:22:10 +0545

    crud done

--- app/crudOpWithFileSys.js
+++ app/crudOpWithFileSys.js
@@ -9,3 +9,14 @@ fs.writeFileSync(filePath, "This is xerox copy.");
 fs.readFile(filePath, 'utf8', (err, f) => {
   console.log(f);
 });
+// update
+// append
+fs.appendFile(filePath,' File is updated.',(err)=>{
+    console.log('file appended.');
+});
+// rename
+fs.rename(filePath, `${dirPath}/demoRenamed.txt`,(err)=>{
+    console.log(`${filePath} is renamed to demoRenamed.txt`);
+});
+// delete
+// fs.unlink(`${dirPath}/demoRenamed.txt`);
