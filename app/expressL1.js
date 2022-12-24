795be44d8b868cc2fe1f6399728329c132ce5684
let_bibek <kumarbibek094@gmail.com>
2022-12-24 15:18:48 +0545

    express page

--- app/expressL1.js
+++ app/expressL1.js
@@ -1,9 +1,22 @@
 const express=require('express');
 const app=express();
+
 app.get('',(req,res)=>{
-    res.send('This a homeeee page.');
+    res.send('<em>This a home page.</em>');
 });
+
 app.get('/demo',(req,resp)=>{
-    resp.send('This is demo page.')
+    resp.send(`<input type="text" name="username" placeholder="Username"/>
+    <button>Send</button>
+    `)
+   
 })
+app.get('/help',(req,res)=>{
+res.send({
+    name:'bibek',
+    username:'let-bibek',
+    email:'bibek@softdrax.com'
+})
+})
+
 app.listen(8200);
