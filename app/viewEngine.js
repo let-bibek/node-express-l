dcd42134a726b4a6fb92ac012f96b58893f00553
let_bibek <kumarbibek094@gmail.com>
2022-12-24 22:35:57 +0545

    dynamic page

--- app/viewEngine.js
+++ app/viewEngine.js
@@ -10,6 +10,34 @@ app.get('/about',(req,res)=>{
     res.sendFile(`${fileDir}/about.html`)
 })
 app.get('/profile',(req,res)=>{
-    res.render('profile');
+    const menu = [
+      {
+        id: 1,
+        title: "buttermilk pancakes",
+        category: "breakfast",
+        price: 15.99,
+        img: "./images/item-1.jpeg",
+        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+      },
+      {
+        id: 2,
+        title: "diner double",
+        category: "lunch",
+        price: 13.99,
+        img: "./images/item-2.jpeg",
+        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+      },
+      {
+        id: 3,
+        title: "godzilla milkshake",
+        category: "shakes",
+        price: 6.99,
+        img: "./images/item-3.jpeg",
+        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+      },
+      
+    ];
+    res.render('profile',{menu});
+
 })
 app.listen(8200);
